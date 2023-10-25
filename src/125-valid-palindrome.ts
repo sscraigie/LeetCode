4926429e47721b2627c0159833131c5d5c8368dd
Spencer Craigie <sscraigie@live.com>
2023-10-25 14:29:59 -0700

    Solution for 125

---
+++ src/125-valid-palindrome.ts
@@ -0,0 +1,23 @@
+//----------Solution 1----------//
+//Speed: 54.32% 70ms
+//Memory: 93.41% 52.6MB
+
+function isPalindrome(s: string): boolean {
+  const string = s.replace(/[.,\/#!$%\^&\*;:{}=@\-_` ~()]/g, "").toLowerCase();
+  if (string.length < 2) return true;
+  let p1 = 0;
+  let p2 = string.length - 1;
+
+  while (p1 < p2) {
+    if (string[p1] != string[p2]) {
+      return false;
+    }
+    p1++;
+    p2--;
+  }
+  return true;
+}
+
+isPalindrome("A man, a plan, a canal: Panama"); // true
+isPalindrome("race a car"); // false
+isPalindrome(" "); //true
