d3f9401506701abc936f59d9ff3e340e56495803
Spencer Craigie <sscraigie@live.com>
2023-11-20 08:43:09 -0800

    Solution for 11

---
+++ src/11-container-with-most-water.ts
@@ -0,0 +1,98 @@
+//----------Solution 1----------//
+//Speed: 74ms 37.38%
+//Memory: 51.34MB 50.53%
+
+// function maxArea(height: number[]): number {
+//   let p1 = 0;
+//   let p2 = height.length - 1;
+//   let hightestArea = 0;
+
+//   while (p1 < p2) {
+//     let w = p2 - p1;
+//     let h;
+//     if (height[p1] > height[p2]) {
+//       h = height[p2];
+//       p2--;
+//     } else {
+//       h = height[p1];
+//       p1++;
+//     }
+//     const area = h * w;
+//     if (hightestArea < area) {
+//       hightestArea = area;
+//     }
+//   }
+//   return hightestArea;
+// }
+
+//----------Solution 2----------//
+//Speed: % MS
+//Memory: % MB
+
+// function maxArea(height: number[]): number {
+//   // let p1 = 0;
+//   // let p2 = height.length - 1;
+//   // let hightestArea; //= p1 - p2 * Math.min(height[p1], height[p2]);
+
+//   console.log(`/---${height}---/`);
+
+//   const p1Map = height.map((value, index) => value + height.length - index);
+//   console.log("p1Map", p1Map);
+//   const p2Map = height.map((value, index) => value + index);
+//   console.log("p2Map", p2Map);
+
+//   const p1Index = p1Map.indexOf(Math.max(...p1Map));
+//   console.log("max", Math.max(...p1Map));
+//   console.log("p1Index", p1Index);
+//   const p2Index = p2Map.lastIndexOf(Math.max(...p2Map));
+//   console.log("max", Math.max(...p2Map));
+//   console.log("p2Index", p2Index);
+
+//   console.log(
+//     "result:",
+//     (p2Index - p1Index) * Math.min(height[p1Index], height[p2Index])
+//   );
+//   console.log(
+//     `${p2Index} - ${p1Index} * (${height[p1Index]} | ${
+//       height[p2Index]
+//     })=>${Math.min(height[p1Index], height[p2Index])}))`
+//   );
+//   return (p2Index - p1Index) * Math.min(height[p1Index], height[p2Index]);
+// }
+
+function maxArea(height: number[]): number {
+  let p1 = 0;
+  let p2 = height.length - 1;
+  let hightestArea = 0;
+
+  while (p1 < p2) {
+    let w = p2 - p1;
+    let h;
+    if (height[p1] > height[p2]) {
+      h = height[p2];
+      let p2Temp = p2--;
+      while (height[p2Temp] < height[p2]) {
+        p2Temp--;
+      }
+    } else {
+      h = height[p1];
+      let p1Temp = p1++;
+      while (height[p1Temp] < height[p1]) {
+        p1Temp++;
+      }
+      p1++;
+    }
+    const area = h * w;
+    if (hightestArea < area) {
+      hightestArea = area;
+    }
+  }
+  return hightestArea;
+}
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])); //49
+console.log(maxArea([1, 1])); //1
+console.log(maxArea([4, 3, 2, 1, 4])); //16
+console.log(maxArea([1, 2, 1])); //2
+console.log(maxArea([1, 2, 4, 3])); //4
+console.log(maxArea([2, 3, 4, 5, 18, 17, 6])); //17
